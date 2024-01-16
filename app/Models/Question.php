a3659fb2f17d6e0097aad60f05327568d4ef5617
Sushantpoojary14 <spoojari694@gmail.com>
2024-01-16 15:04:26 +0530

    ,

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -58,7 +58,7 @@ class Question extends Model
     protected function option1(): Attribute
     {
         return Attribute::make(
-            set: fn(string $value) => rtrim($value, ',')
+            set: fn(string $value) => rtrim($value, ',') ?? null
 
         );
     }
@@ -81,20 +81,20 @@ class Question extends Model
     protected function option2(): Attribute
     {
         return Attribute::make(
-            set: fn(string $value) => rtrim($value, ',')
+            set: fn(string $value) => rtrim($value, ',') ?? null
         );
     }
     protected function option3(): Attribute
     {
         return Attribute::make(
-            set: fn(string $value) => rtrim($value, ',')
+            set: fn(string $value) => rtrim($value, ',') ?? null
         );
     }
 
     protected function option4(): Attribute
     {
         return Attribute::make(
-            set: fn(string $value) => rtrim($value, ',')
+            set: fn(string $value) => rtrim($value, ',') ?? null
         );
     }
 
