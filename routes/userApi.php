c2d34cc060fad51566f0b90d1188eb5400f35c83
Sushantpoojary14 <spoojari694@gmail.com>
2024-01-25 11:40:27 +0530

    added user id

--- routes/userApi.php
+++ routes/userApi.php
@@ -36,7 +36,7 @@ Route::group(['middleware' => ['jwt.role:users','jwt.auth']],function ()
     Route::get('/get-user-purchases/{ts_id}',[ProductController::class,'getTSPurchases']);
     Route::get('/get-user-test-details/{uts_id}',[ProductController::class,'getTSDetails']);
     Route::get('/check-user-purchase-expire/{user_id}',[ProductController::class,'checkUserPurchaseExpire']);
-    Route::get('/get-latest-product',[ProductController::class,'getLatestProduct']);
+    Route::get('/get-latest-product/{user_id}',[ProductController::class,'getLatestProduct']);
 
     //user test schedule
     Route::post('/post-user-test-status',[UserController::class,'userTestStatus']);
